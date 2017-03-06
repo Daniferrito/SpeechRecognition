ba6ee84a177e3f27bfc300cf7af946cb46214467
Jesús Tomás <jtomas@upv.es>
2017-03-06 18:28:34 +0100

    Clase CardGrammar junto con CocheGrammar

--- common/src/main/java/com/daferto/common/CarGrammar.java
+++ common/src/main/java/com/daferto/common/CarGrammar.java
@@ -7,6 +7,20 @@ package com.daferto.common;
 public class CarGrammar extends Grammar{
 
 /*
+      hood                  grid 1|2|3..8       completed
+      roof                  <nada>              incompleted
+      box                                       add sag
+      tailgate                                  add mottle
+      left door
+      right door
+      both door
+      left fender
+      right fender
+      both fender
+      alfa pillar
+      bravo pillar
+      charlie pillar
+
       capó                  grid 1|2|3..8       completo
       techo                 <nada>              defecto
       box                                       añade bollo
@@ -27,48 +41,40 @@ public class CarGrammar extends Grammar{
     public CarGrammar(String lang){
         super(100); //100 = tamaño máximo de vocabulario
         if(lang.equals("en-US")) {
+            newRule("<INI>","<PIECE>","hood","roof","box","tailgate");
+            newRule("<INI>","<LEFT_RIGHT>","left","right","both");
+            newRule("<LEFT_RIGHT>","<PIECE>","door", "fender");
+            newRule("<INI>","<PILLAR>","alfa","bravo","charlie");
+            newRule("<PILLAR>","<PIECE>","pillar");
+
+            newRule("<PIECE>","<GRID>","grid");
+            newRule("<GRID>", "<NUM>", "one", "two", "three", "four", "five", "six", "seven", "eight");
+            newRule("<PIECE>","<NUM>","");
+
+            newRule("<NUM>","<END>","completed","incompleted");
+            newRule("<NUM>","<ADD>","add");
+            newRule("<ADD>","<END>","sag","mottle");
+        } else {
             newRule("<INI>","<PIEZA>","capó","techo","box");
             newRule("<INI>","<PIEZA>","capó","techo","box");
-            newRule("<INI>","<PUERTA>","puerta");
-            newRule("<PUERTA>","<PIEZA>","derecha","izquierda");
-            newRule("<INI>","<ALETA>","aleta");
-            newRule("<ALETA>","<PIEZA>","derecha","izquierda");
+            newRule("<INI>","<PUERTA_ALETA>","puerta","aleta");
+            newRule("<PUERTA_ALETA>","<PIEZA>","derecha","izquierda");
             newRule("<INI>","<PORTÓN>","portón");
             newRule("<PORTÓN>","<PIEZA>","box");
             newRule("<INI>","<AMBAS>","ambas");
             newRule("<AMBAS>","<PIEZA>","puertas","aletas");
             newRule("<INI>","<PILAR>","pilar");
             newRule("<PILAR>","<PIEZA>","a","b","c");
+
             newRule("<PIEZA>","<GRID>","grid");
             newRule("<GRID>", "<NUM>", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho");
-            newRule("<PIEZA>","<END>","completo","defecto","añade bollo","añade mancha");
-            newRule("<NUM>","<END>","completo","defecto","añade bollo","añade mancha");
-            newRule("<PIEZA>","<AÑADE>","añade");
-            newRule("<NUM>","<AÑADE>","añade");
-            newRule("<AÑADE>","<END>","bollo","mancha");
-        } else {
-            newRule("<INI>", "<PIEZA>", "hood", "roof", "box");
-            newRule("<INI>", "<PIEZA>", "capó", "techo", "box");
-            newRule("<INI>", "<PUERTA>", "puerta");
-            newRule("<PUERTA>", "<PIEZA>", "derecha", "izquierda");
-            newRule("<INI>", "<ALETA>", "aleta");
-            newRule("<ALETA>", "<PIEZA>", "derecha", "izquierda");
-            newRule("<INI>", "<PORTÓN>", "portón");
-            newRule("<PORTÓN>", "<PIEZA>", "box");
-            newRule("<INI>", "<AMBAS>", "ambas");
-            newRule("<AMBAS>", "<PIEZA>", "puertas", "aletas");
-            newRule("<INI>", "<PILAR>", "pilar");
-            newRule("<PILAR>", "<PIEZA>", "a", "b", "c");
-
-            newRule("<PIEZA>", "<GRID>", "grid");
-            newRule("<GRID>", "<NUM>", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho");
-            newRule("<PIEZA>", "<NUM>", "");
+            newRule("<PIEZA>","<NUM>","");
 
 //        newRule("<PIEZA>","<END>","completo","defecto","añade bollo","añade mancha");
-            newRule("<NUM>", "<END>", "completo", "defecto", "añade bollo", "añade mancha");
+            newRule("<NUM>","<END>","completo","defecto");
 //        newRule("<PIEZA>","<AÑADE>","añade");
-            newRule("<NUM>", "<AÑADE>", "añade");
-            newRule("<AÑADE>", "<END>", "bollo", "mancha");
+            newRule("<NUM>","<AÑADE>","añade");
+            newRule("<AÑADE>","<END>","bollo","mancha");
         }
     }
 
